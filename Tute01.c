0964ecd449e76dda6ee150df75b07f07ff6f31f6
IT21329760 <94285143+IT21329760@users.noreply.github.com>
2022-02-16 10:50:38 +0000

    calculate avg

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,18 @@
 #include <stdio.h>
 
 int main() {
+  float no1,no2;
+  float avg;
+
+  printf("Enter number 01: ");
+  scanf("%f",&no1);
   
+  printf("Enter number 02: ");
+  scanf("%f",&no2);
+  
+
+  avg = (no1 + no2)/2.0;
+  printf("Average is: %f ",avg);
   return 0;
 }
 
